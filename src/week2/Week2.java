0fd4a5aa296b757aff50fdbfb6eee8b0c30b6c5d
HP <HP@DESKTOP-AF6UDPR>
2020-04-10 14:52:44 +0700

    V1.0

---
+++ src/week2/Week2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2;
+
+/**
+ *
+ * @author HP
+ */
+public class Week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ICaculate calculator = new CasioCaculator();
+        System.out.println(calculator.cong(4, 5));
+    }
+    
+}
